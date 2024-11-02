707fbf779abf978628db594dcb71e0edd0217c9c
Fernando Thomas Schmidt <30754621+f-thms@users.noreply.github.com>
2024-11-01 21:28:00 -0300

    Fix tab size

--- src/server.js
+++ src/server.js
@@ -1,29 +1,29 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import { routes  } from './routes.js'
+import { routes } from './routes.js'
 import { extractQueryParams } from './utils/extract-query-params.js'
 
-const server  = http.createServer(async (req, res) => { 
-    const {method, url} = req
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
 
-    await json(req, res) 
+  await json(req, res)
 
-    const route = routes.find(route => {
-        return route.method === method && route.path.test(url)
-    })
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    if(route) {
-        const routeParams = req.url.match(route.path)
-        
-        const { query, ...params } = routeParams.groups
+  if (route) {
+    const routeParams = req.url.match(route.path)
 
-        req.params = params
-        req.query = query ? extractQueryParams(query) : {}
+    const { query, ...params } = routeParams.groups
 
-        return route.handler(req, res)
-    }
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
-    res.end("ok");
+    return route.handler(req, res)
+  }
+
+  res.end("ok");
 })
 
 server.listen(3333)
